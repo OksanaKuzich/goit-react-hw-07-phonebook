4cd22e3bd82353d4827d217024eff1555e43a0ec
OksanaKuzich <komoks255@gmail.com>
2022-12-05 13:50:41 +0200

    start file

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+// import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { GlobalStyle } from './Utilit/GlobalStyle';
+import { getContactsArray } from 'redux/selectors';
+import { useSelector } from 'react-redux';
+
+
+export const App = () => {
+  const array = useSelector(getContactsArray);
+
+  return (
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {array.length === 0 ? (
+        <p>Please, enter your first contact</p>
+      ) : (
+        <>
+          <Filter />
+          <ContactList />
+        </>
+      )}
+
+      <GlobalStyle />
+    </div>
+  );
+};
